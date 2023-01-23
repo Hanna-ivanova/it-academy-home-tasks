94ae5e5329476a5e5bdfbfff31fe978dbddb632f
Hanna Ivanova <hanna.ivanovadl@gmail.com>
2023-01-23 20:30:45 +0300

    hometask#1

---
+++ index.js
@@ -0,0 +1,5 @@
+// task 1  the easier is the better, if needed i can try to do it in a bit complicated way, just let me know
+let str = "#";
+for (let i = str.length; i < 8; i++) {
+    console.log(str.repeat(i)); 
+}
