7e455739554b532d1e3815354d5d101ad29116cb
Hanna Ivanova <hanna.ivanovadl@gmail.com>
2023-01-24 14:09:05 +0300

    task#2 done

--- index.js
+++ index.js
@@ -3,3 +3,15 @@ let str = "#";
 for (let i = str.length; i < 8; i++) {
     console.log(str.repeat(i)); 
 }
+
+//task 2
+let chessline = "";
+const uneven = "#"
+const even = " ";
+while (chessline.length < 64) {
+    chessline += uneven + even;
+ }
+ for (let i = 1; i < 4; i++) {
+    console.log (chessline.substring(1, 9));
+    console.log (chessline.substring(2, 9));
+}
