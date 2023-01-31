1b3166aa4e3f035ae26a15fcfc4e81cd34cb2a6b
Hanna Ivanova <hanna.ivanovadl@gmail.com>
2023-01-31 16:36:34 +0300

    e-mail validation

--- index.js
+++ index.js
@@ -27,3 +27,22 @@ do {
   someNumber = prompt("Enter any number greater than 100");
 }
 while (someNumber < 100  && someNumber);
+
+//03-homework
+let array = [
+  'mail@mail.312412',
+  'mail213_123@mail.com',
+  '_@mail.ru',
+  '324mail@mail.ru',
+  't@test.ru'
+] 
+
+function validateEmail(email) {
+  const re = /\w+@[a-zA-Z0-9.-]+?\.[a-zA-Z]{2,6}/; //i have found so many regular expressions for e-mail validation, have a clue what every sign stands for, but no idea what the requirements for validations are
+  return re.test(email);
+}
+    
+
+    for (let i=0; i< array.length; i++) {
+      console.log(validateEmail(array[i]));
+    }
